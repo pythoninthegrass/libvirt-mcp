3633afb05ca6a46295332559f64cfe5512dee855
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2025-07-13 23:31:45 -0500

    chore (server.py): sort imports, pep8 formatting

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
-from mcp.server.fastmcp import FastMCP
 from handlers import register_handlers
+from mcp.server.fastmcp import FastMCP
 
 # Create an MCP server
 mcp = FastMCP("libvirt-mcp-demo")
 
 register_handlers(mcp)
 
+
 def main():
     print("Hello from libvirt-mcp-demo!")
+
 
 if __name__ == "__main__":
     mcp.run(transport="stdio")
